00cb3586b7d4da774dd68ed4de9f7675fe54e8c9
Yvz-DevOps <68934275+Yvz-DevOps@users.noreply.github.com>
2020-08-21 19:48:25 -0400

    Update public_subnet.tf

--- public_subnet.tf
+++ public_subnet.tf
@@ -1,21 +1,25 @@
-resource "aws_subnet" "private1" {
+  
+resource "aws_subnet" "public1" {
   vpc_id            = "${aws_vpc.main.id}"
-  cidr_block        = "${var.private_cidr1}"
+  cidr_block        = "${var.public_cidr1}"
   availability_zone = "${var.region}a"
+  map_public_ip_on_launch = true                      
   tags              =   "${var.tags}"
 
 }
-resource "aws_subnet" "private2" {
+resource "aws_subnet" "public2" {
   vpc_id            = "${aws_vpc.main.id}"
-  cidr_block        = "${var.private_cidr2}"
+  cidr_block        = "${var.public_cidr2}"
   availability_zone = "${var.region}b"
-    tags              =   "${var.tags}"
+  map_public_ip_on_launch = true                      
+  tags              =   "${var.tags}"
 
 }
-resource "aws_subnet" "private3" {
+resource "aws_subnet" "public3" {
   vpc_id            = "${aws_vpc.main.id}"
-  cidr_block        = "${var.private_cidr3}"
+  cidr_block        = "${var.public_cidr3}"
   availability_zone = "${var.region}c"
-    tags              =   "${var.tags}"
+  map_public_ip_on_launch = true                     
+  tags              =   "${var.tags}"
 
 }
