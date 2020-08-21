a4834c6247421a56920004b053ed195da55ae8fa
Yvz-DevOps <68934275+Yvz-DevOps@users.noreply.github.com>
2020-08-21 19:43:29 -0400

    Create null.tf

---
+++ null.tf
@@ -0,0 +1,18 @@
+resource "null_resource"  "commands1" {
+    depends_on = ["aws_instance.vpc_task_db"]
+    triggers = {
+        always_run = "${timestamp()}"
+    }
+
+    provisioner "remote-exec" {
+        connection {
+        host = "${aws_instance.vpc_task_db.public_ip}"
+        type = "ssh"
+        user = "ec2-user"
+        private_key = "${file("~/.ssh/id_rsa")}"
+    }
+    inline = [
+        "sudo yum install mariadb-server mariadb -y", 
+        "sudo systemctl start mariadb", 
+        "sudo systemctl enable  mariadb", 
+}
