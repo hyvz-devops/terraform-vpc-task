9ea9c508491f6c7060640891ce400b83cc8b0b71
Yvz-DevOps <68934275+Yvz-DevOps@users.noreply.github.com>
2020-08-21 21:08:30 -0400

    Update sec_group.tf

--- sec_group.tf
+++ sec_group.tf
@@ -29,27 +29,7 @@ resource "aws_security_group" "vpc_task" {
     vpc_id = "${aws_vpc.main.id}"
 
 }
-
-resource "aws_security_group" "vpc_task_db" {
-  name        = "vpc_task_db"
-  description = "Allow TLS inbound traffic"
-
-  ingress {
-    description = "Allow mysql"
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-ingress {
-    description = "Allow ssh"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
+ 
   ingress {
     description = "TLS from VPC"
     from_port   = 8080
@@ -58,13 +38,6 @@ ingress {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-    tags              =   "${var.tags}"
     
     vpc_id = "${aws_vpc.main.id}"
 
